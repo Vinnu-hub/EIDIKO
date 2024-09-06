2b4da4b52c67ed60c6397b751d99862da7e767d7
Vinnu-hub <gugulothvinay.eidiko@gmail.com>
2024-09-06 18:40:46 +0530

    Add files via upload
    This code is to read emails from our Gmail account based on the subject. In the received email, our Gmail address should be present in the CC field.

---
+++ demo/src/main/java/com/example/demo/config/EmailController.java
@@ -0,0 +1,29 @@
+package com.example.demo.config;
+
+//import mport com.youtube.jwt.service.GmailReader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/emails")
+public class EmailController
+{
+
+    @Autowired
+    private GmailReader gmailReader;
+
+
+
+    @GetMapping("/read")
+    public String fetchEmails()
+    {
+        try
+        {
+            return gmailReader.fetchEmails();
+        } catch (Exception e) {
+            return "Error fetching emails: " + e.getMessage();
+        }
+    }
+}
